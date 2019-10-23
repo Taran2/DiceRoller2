fe020a9c6b5bc072674c8c19b8a662cdae6bd3c4
Taran <1707273@brunel.ac.uk>
2019-10-23 19:24:08 +0100

    Added user input, bounds and congratulations

--- app/src/main/java/com/example/diceroller/MainActivity.java
+++ app/src/main/java/com/example/diceroller/MainActivity.java
@@ -1,5 +1,40 @@
 package com.example.diceroller;
 
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import java.util.Random;
+
+
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import java.util.Random;
+
+
+
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -12,15 +47,36 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Random;
 
+
 public class MainActivity extends AppCompatActivity {
+
+    private Button validate;
+    private Object numbertofind;
+    private int counter = 0;
+    private TextView a;
+    private TextView TN;
+    private EditText Input_Num;
+    private TextView G;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        G = (TextView) findViewById(R.id.msg);
+        Input_Num = (EditText) findViewById(R.id.numberEntered);
+        a = (TextView) findViewById(R.id.counter);
+        TN = (TextView) findViewById(R.id.cview);
+
+
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -65,5 +121,16 @@ public class MainActivity extends AppCompatActivity {
 
         tv.setText(Integer.toString(number));
 
+        int n = Integer.parseInt(Input_Num.getText ().toString());
+
+            if (n < 1 || n > 6){
+                Toast.makeText(this, "Invalid Input", Toast.LENGTH_SHORT);
+            } else if (n == number){
+                Toast.makeText(this, "Congratulations", Toast.LENGTH_SHORT);
+
+                counter = counter +1;
+
+                a.setText(Integer.toString(counter));
+            }
     }
 }
